5ffc47491eb80d049c7eb8d47d09e35d0ae972aa
Chun Fan <4866953+chunfantasy@users.noreply.github.com>
2023-10-27 14:14:53 +0200

    Update

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,7 +2,9 @@
 
 -- Fuzzy finder with telescope
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>ff",
+  ":lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
+  {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -74,24 +74,24 @@ return {
 
         -- Buffer local mappings.
         -- See `:help vim.lsp.*` for documentation on any of the below functions
-        -- local opts = { buffer = ev.buf }
-        -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-        -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-        -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        -- local local_opts = { buffer = ev.buf }
+        -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, local_opts)
+        -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, local_opts)
+        -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, local_opts)
+        -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, local_opts)
+        -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, local_opts)
+        -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, local_opts)
+        -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, local_opts)
         -- vim.keymap.set('n', '<space>wl', function()
         --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        -- end, opts)
-        -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-        -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        -- vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-        -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        -- end, local_opts)
+        -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, local_opts)
+        -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, local_opts)
+        -- vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, local_opts)
+        -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, local_opts)
         vim.keymap.set('n', 'ff', function()
           vim.lsp.buf.format { async = true }
-        end, opts)
+        end, local_opts)
       end,
     })
 
